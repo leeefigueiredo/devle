228ef63a6fd85547a354e0f176e7fc0a14af70de
leeeandrofigueiredo <leeeandrofigueiredo@gmail.com>
2024-09-12 15:36:37 -0300

    ok

--- 03_Excel.js
+++ 03_Excel.js
@@ -54,4 +54,13 @@ function pegarValor() {
 }
 
 
+// pop-up.js
+function mostrarPopup() {
+    document.getElementById('popup').style.display = 'block';
+}
+
+function fecharPopup() {
+    document.getElementById('popup').style.display = 'none';
+}
+
 
