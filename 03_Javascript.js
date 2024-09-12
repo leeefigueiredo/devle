d17684f6b0bd072bba1d64295c94f12b051f463e
leeeandrofigueiredo <leeeandrofigueiredo@gmail.com>
2024-09-12 15:03:43 -0300

    ok

--- 03_Javascript.js
+++ 03_Javascript.js
@@ -2,9 +2,9 @@
 
 const dataAtual = new Date();
 const anoAtual = dataAtual.getFullYear();
-outputIdade.innerHTML = anoAtual - 1993; 
+outputIdade.innerHTML = anoAtual - 1993;
 
 const dataAtual2 = new Date();
 const anoAtual2 = dataAtual2.getFullYear();
-outputTime.innerHTML = anoAtual - 2022; 
+outputTime.innerHTML = anoAtual - 2022;
 
